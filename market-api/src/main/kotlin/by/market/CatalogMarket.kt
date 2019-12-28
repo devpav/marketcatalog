000547f62dfa8b808b303e8b79f0f9bce0be7862
devpav <jsdeveloper@yahoo.com>
2019-12-28 19:18:30 +0300

    #22: Add docker [nginx, postgres, kotlin]

--- market-api/src/main/kotlin/by/market/CatalogMarket.kt
+++ market-api/src/main/kotlin/by/market/CatalogMarket.kt
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class CatalogMarket
-
-fun main(args: Array<String>) {
-    SpringApplication.run(CatalogMarket::class.java, *args)
+class CatalogMarket {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            SpringApplication.run(CatalogMarket::class.java, *args)
+        }
+    }
 }
 
