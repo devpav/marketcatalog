bab17de932b8442e6f97f4ee73bb22529a674a41
devpav <jsdeveloper@yahoo.com>
2020-01-11 17:03:19 +0300

    #56: Fix repository method for findByMetadataAndProductRowId

--- market-api/src/main/kotlin/by/market/repository/characteristic/single/DoubleSingleCharacteristicRepository.kt
+++ market-api/src/main/kotlin/by/market/repository/characteristic/single/DoubleSingleCharacteristicRepository.kt
@@ -7,6 +7,4 @@ import org.springframework.stereotype.Repository
 @Repository
 interface DoubleSingleCharacteristicRepository
     : AbstractSingleCharacteristicRepository<DoubleCharacteristic, Double> {
-
-
 }
