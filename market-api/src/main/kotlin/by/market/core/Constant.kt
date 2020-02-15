8f2f3cb55809f616618bcb14b1bae9289f23f9cc
blowin <zk.,k.fk`ye19>
2020-02-15 20:54:00 +0300

    #94 add DataType and Category constants

--- market-api/src/main/kotlin/by/market/core/Constant.kt
+++ market-api/src/main/kotlin/by/market/core/Constant.kt
@@ -1,12 +1,40 @@
 package by.market.core
 
 object Constant {
-    // TODO добавить типы, категории , и другие известные значения
-
-    object EntityMetadata{
+    object EntityMetadata {
         val Accessory: String = "accessory"
         val Cornice: String = "cornice"
         val Jalousie: String = "jalousie"
         val Rolstor: String = "rolstor"
+    }
+
+    object DataType {
+        val String: String = "STRING"
+        val Double: String = "DOUBLE"
+    }
+
+    object Category {
+       val Cornice: String = "cornice"
+       val Metallic: String = "metallic"
+       val PlasticCeilings: String = "plastic_ceilings"
+       val WallMetalPlastic: String = "wall_metal_plastic"
+       val Flexible: String = "flexible"
+       val AccessoriesForCeiling: String = "accessories_for_ceiling"
+       val MetalPlasticAccessories: String = "metal_plastic_accessories"
+       val AccessoriesForMetal: String = "accessories_for_metal"
+
+       val Rolstor: String = "rolstor"
+       val DayNight: String = "day_night"
+       val Standard: String = "standard"
+       val InBox: String = "in_box"
+       val Premium: String = "premium"
+       val Blackout: String = "blackout"
+
+       val Jalousie: String = "jalousie"
+
+       val Accessory: String = "accessory"
+       val Luversa: String = "luversa"
+       val GripsHoldersHooks: String = "grips_holders_hooks"
+       val MagneticClips: String = "magnetic_clips"
     }
 }
