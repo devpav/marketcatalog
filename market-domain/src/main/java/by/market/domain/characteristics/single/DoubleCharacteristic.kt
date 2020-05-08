a463eb9eb7af2deb04aaf85ec2f2c41af5c2151e
a18216891 <devpavdeveloper@gmail.com>
2020-05-08 22:29:37 +0300

    #115: Add indexs for characteristic value

--- market-domain/src/main/java/by/market/domain/characteristics/single/DoubleCharacteristic.kt
+++ market-domain/src/main/java/by/market/domain/characteristics/single/DoubleCharacteristic.kt
@@ -2,14 +2,8 @@ package by.market.domain.characteristics.single
 
 import by.market.domain.characteristics.AbstractCharacteristic
 import javax.persistence.Entity
-import javax.persistence.Index
 import javax.persistence.Table
 
-@Table(
-        name = "TBX_CH_DOUBLE_CHARACTERISTIC",
-        indexes = [
-            Index(name = "double_characteristic_id_entity_metadata_id_product_row_idx", columnList = "FK_ENTITY_METADATA, FK_PRODUCT")
-        ]
-)
-@Entity()
+@Entity
+@Table(name = "TBX_CH_DOUBLE_CHARACTERISTIC")
 class DoubleCharacteristic : AbstractCharacteristic<Double>()
--- market-domain/src/main/java/by/market/domain/characteristics/single/StringCharacteristic.kt
+++ market-domain/src/main/java/by/market/domain/characteristics/single/StringCharacteristic.kt
@@ -2,14 +2,8 @@ package by.market.domain.characteristics.single
 
 import by.market.domain.characteristics.AbstractCharacteristic
 import javax.persistence.Entity
-import javax.persistence.Index
 import javax.persistence.Table
 
 @Entity
-@Table(
-    name = "TBX_CH_STRING_CHARACTERISTIC",
-    indexes = [
-        Index(name = "string_characteristic_id_entity_metadata_id_product_row_idx", columnList = "FK_ENTITY_METADATA, FK_PRODUCT")
-    ]
-)
+@Table(name = "TBX_CH_STRING_CHARACTERISTIC")
 class StringCharacteristic : AbstractCharacteristic<String>()
