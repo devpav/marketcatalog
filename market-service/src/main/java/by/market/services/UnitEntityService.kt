d053d9c6d1b973fdadcdcadb926c617e8dd533a4
a18216891 <devpavdeveloper@gmail.com>
2020-05-09 10:04:16 +0300

    #117: Add service for UnitEntity.kt

---
+++ market-service/src/main/java/by/market/services/UnitEntityService.kt
@@ -0,0 +1,9 @@
+package by.market.services
+
+import by.market.domain.units.UnitEntity
+
+interface UnitEntityService : IService<UnitEntity> {
+
+    fun findByValue(value: String): UnitEntity?
+
+}
